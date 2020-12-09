827141ae1b081a2e7eadd7d96ca79ca9d21648bb
Vatcharin Kongsakul <ghinryudokung@gmail.com>
2020-12-09 16:11:29 +0700

    first initial vue

---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3 mt-3 class="text-center">
+        <h1>Welcome to Awesome App</h1>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 mt-3>
+        <blockquote class="blockquote text-center">
+          It's a basic Single Page application <br>
+          with MUT Mini contest Event
+        </blockquote>
+      </v-flex>
+      <v-flex xs12 sm6 offset-sm3 class="text-center">
+        <v-btn color="primary" to="/signup">Sign Up</v-btn>
+        <v-btn to="/signin">Sign In</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+export default {
+  //name: "Landing"
+};
+</script>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <v-flex xs12 class="text-center" mt-5>
+        <v-img
+          :src="require('../assets/404.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+        <h1>Error - page not found</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+export default {};
+</script>
