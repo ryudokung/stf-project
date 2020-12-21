a6a0677354c51ba2a8ac271602d9549567c35d04
Vatcharin Kongsakul <ghinryudokung@gmail.com>
2020-12-21 13:18:31 +0700

    add feature/login

---
+++ src/components/SignIn.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+     <v-card height="500px"></v-card>
+      <v-flex xs12 sm6 offset-sm3 mt-3 class="text-center">
+        <v-container style="position: relative;top: 13%;" class="text-xs-center">
+          <v-card flat>
+            <v-card-title primary-title>
+              <h4>Login</h4>
+            </v-card-title>
+            <v-form>
+            <v-text-field prepend-icon="person" name="Username" label="Username"></v-text-field>
+            <v-text-field prepend-icon="lock" name="Password" label="Password" type="password"></v-text-field>
+            <v-card-actions>
+              <v-btn primary large block>Login</v-btn>
+            </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
+import 'font-awesome/css/font-awesome.min.css'
 
 const routerOptions = [
   { path: "/", component: "Hello" },
   { path: "/helloworld", component: "HelloWorld" },
   { path: "/scoreboard", component: "ScoreBoard" },
+  { path: "/signin", component: "SignIn" },
 //   { path: "/signin", component: "SignIn" },
 //   { path: "/signup", component: "SignUp" },
 //   { path: "/home", component: "Home", meta: { requiresAuth: true } },
